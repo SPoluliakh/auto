0058f08bda9ca84ed0b307ff71f6dcab8c8b9dd2
Dmitriy Matv <johndoe@example.com>
2024-07-15 02:30:23 +0300

    [add] top navigation

--- src/common/components/Header/Header.jsx
+++ src/common/components/Header/Header.jsx
@@ -1,38 +1,39 @@
-import { headerNav } from '../helpers/headerNav'
+import { headerNav } from "../helpers/headerNav";
 import { IoIosArrowDown } from "react-icons/io";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { RxCrossCircled } from "react-icons/rx";
-import * as SC from './Header.styled'
-
+import * as SC from "./Header.styled";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
-	return	<SC.Wrapper>
-				<SC.LogoWrapper>
-					<SC.Logo>
-						<a href="">
-							AutoParser
-						</a>
-					</SC.Logo>
-				</SC.LogoWrapper>
-				<SC.ListWrapper>
-					<SC.List>
-						{headerNav.map(({id, title, href}) => 
-							<li key={id}>
-								<SC.Button>
-									<SC.Text>
-										{title}
-									</SC.Text>
-									<IoIosArrowDown/>
-								</SC.Button>
-							</li>)}
-					</SC.List>
-				</SC.ListWrapper>
-				<SC.BurgerWrapper>	
-					<SC.BurgerInnerWrapper>
-						<SC.BurgerButton>
-							<RxHamburgerMenu size={"21px"}/>
-						</SC.BurgerButton>
-					</SC.BurgerInnerWrapper>
-				</SC.BurgerWrapper>
-			</SC.Wrapper>
-}
+  return (
+    <SC.Wrapper>
+      <SC.LogoWrapper>
+        <SC.Logo>
+          <Link to={"/"}>AutoParser</Link>
+        </SC.Logo>
+      </SC.LogoWrapper>
+      <SC.ListWrapper>
+        <SC.List>
+          {headerNav.map(({ id, title, href }) => (
+            <li key={id}>
+              <SC.Button>
+                <SC.Text>
+                  <Link to={href}>{title}</Link>
+                </SC.Text>
+                <IoIosArrowDown />
+              </SC.Button>
+            </li>
+          ))}
+        </SC.List>
+      </SC.ListWrapper>
+      <SC.BurgerWrapper>
+        <SC.BurgerInnerWrapper>
+          <SC.BurgerButton>
+            <RxHamburgerMenu size={"21px"} />
+          </SC.BurgerButton>
+        </SC.BurgerInnerWrapper>
+      </SC.BurgerWrapper>
+    </SC.Wrapper>
+  );
+};
